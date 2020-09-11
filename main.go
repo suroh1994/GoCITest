5a613ef433153642833c9dfcc138a8520cea086a
Sebastian Jung <sebastian.jung@syrocon.de>
2020-09-11 16:41:16 +0200

    feat(app): Add self-updating demo code

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/blang/semver"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
 )
 
-var version = "0.0.0-src"
+var version = "0.0.0"
 
 func main() {
 	fmt.Printf("hello world, you are running version %s\n", version)
+	doSelfUpdate()
 }
 
 func funnyFunction() string {
 	return "Smile! :)"
+}
+
+func doSelfUpdate() {
+	v := semver.MustParse(version)
+	latest, err := selfupdate.UpdateSelf(v, "suroh1994/GoCITest")
+	if err != nil {
+		log.Println("Binary update failed:", err)
+		return
+	}
+	if latest.Version.Equals(v) {
+		// latest version is the same as current version. It means current binary is up to date.
+		log.Println("Current binary is the latest version", version)
+	} else {
+		log.Println("Successfully updated to version", latest.Version)
+		log.Println("Release note:\n", latest.ReleaseNotes)
+	}
 }
